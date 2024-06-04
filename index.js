ca8fa40b04a84513036d7f88aa64c9cf81a7a160
George <drumerboygt.gt@gmail.com>
2024-06-03 21:31:10 -0400

    Added better authentification for users by hashing passwords. Added Passport.

--- index.js
+++ index.js
@@ -3,44 +3,143 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import axios from "axios";
+import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import env from "dotenv";
 
-//Establishing the database.
-const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "bookreview",
-    password: "Omniverse",
-    port: 5432
-});
-
-//Connecting to the database.
-db.connect();
 
 //Setting up the app and the port number.
 const app = express();
 const port = 3000;
 
-//Placeholder for the username.
-let reviewer = '';
+//Salt Rounds.
+const saltRounds = 10;
+
+//ENV Configuration.
+env.config();
+
+//Establishing the database.
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+
+//Connecting to the database.
+db.connect();
+
 
 //Body-Parser as well as setting up the static route to public.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//Session.
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        maxAge: 1000 * 60 * 60 * 2,
+    }
+}));
+
+//Passport session.
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Get method for the starting page of the app.
 app.get("/", (req, res) => {
     res.render("start.ejs");
 });
 
+app.route("/signin")
+//Sign-in get method
+.get((req, res) => {
+    res.render("signin.ejs");
+})
+//Sign-in post request.
+.post(passport.authenticate("local", {
+    successRedirect: "/homepage",
+    failureRedirect: "/signin",
+}));
+
+
+app.route("/create")
+//Create get method.
+.get((req, res) => {
+    res.render("signin.ejs");
+})
+//Create post request.
+.post(async(req, res) => {
+
+    //Grabs the username and password from the user.
+    const username = req.body.username.trim();
+    const password = req.body.password.trim();  
+
+    //Try Catch checks if the user is in the database.
+    try {
+
+        const result = await db.query("SELECT * FROM users WHERE username = $1", [username]);
+
+        //If there are no results, add the user and password to the users database.
+        if(result.rows.length === 0){
+            
+            bcrypt.hash(password, saltRounds, async(err, hash) => {
+                
+                if(err){
+                    console.log("Error hashing password: ", err);
+                }
+                
+                else{
+                   const result = await db.query(
+                        "INSERT INTO users(username, password) VALUES ($1, $2) RETURNING *",
+                        [username, hash]
+                    );
+
+                    const user = result.rows[0];
+
+                    req.login(user, (err) => {
+                        console.log(err);
+                        res.redirect("/homepage");
+                    });
+
+                }
+                 
+            });
+            
+        }
+
+        //Else, the user already exist in the database.
+        else {
+            res.send("User already exist.");
+        }
+        
+    } catch (error) {
+        res.send("Error adding credentials into database", error);
+    }
+
+});
+
+
 app.route("/homepage")
 //Get method for the homepage.
 .get((req, res) => {
     
-    //Render the homepage.ejs with the user. The user is created from the signin or create post request.
-    res.render("homepage.ejs", {     
-        username: reviewer
-    });
-
+    if(req.isAuthenticated()){
+        
+        //Render the homepage.ejs with the user. The user is created from the signin or create post request.
+        res.render("homepage.ejs", {     
+            username: req.user.username
+        });  
+   
+    } else {
+        res.redirect("/signin");
+    }
+ 
 })
 //Homepage post request.
 .post(async(req, res) => {
@@ -105,7 +204,7 @@ app.route("/homepage")
           
             //Render the homepage.ejs with the user, cover, author, and title for the book.
             res.render("homepage.ejs", {
-                username: reviewer,
+                username: req.user.username,
                 image: cover.config.url,
                 author: resultAuthor,
                 title: resultTitle,
@@ -119,6 +218,7 @@ app.route("/homepage")
 
 });
 
+
 app.route("/reviews")
 //Get method to get all reviews for the user.
 .get(async (req, res) => {
@@ -127,7 +227,7 @@ app.route("/reviews")
     try {
 
         //This query gets the results of all reviews from the selected user.
-        const results = await db.query("SELECT * FROM reviews WHERE username = $1", [reviewer]);
+        const results = await db.query("SELECT * FROM reviews WHERE username = $1", [req.user.username]);
 
         //Empty reviews array that will hold all the results.
         const reviews = [];
@@ -139,7 +239,7 @@ app.route("/reviews")
 
         //Render the reviews.ejs page with the username and the reviews.
         res.render("reviews.ejs", {
-            username: reviewer,
+            username: req.user.username,
             reviews: reviews 
         });
         
@@ -154,7 +254,7 @@ app.route("/reviews")
     try {
 
         //This query gets the results of all reviews from the selected user.
-        const results = await db.query("SELECT * FROM reviews WHERE username = $1", [reviewer]);
+        const results = await db.query("SELECT * FROM reviews WHERE username = $1", [req.user.username]);
 
         //Empty reviews array that will hold all the results.
         const reviews = [];
@@ -166,7 +266,7 @@ app.route("/reviews")
 
         //Render the reviews.ejs page with the username and the reviews.
         res.render("reviews.ejs", {
-            username: reviewer,
+            username: req.user.username,
             reviews: reviews 
         });
         
@@ -175,6 +275,7 @@ app.route("/reviews")
     }
 
 });
+
 
 //Starting screen's post request.
 app.post("/start", (req, res) => {
@@ -191,102 +292,6 @@ app.post("/start", (req, res) => {
  
 });
 
-//Sign-in post request.
-app.post("/signin", async(req, res) => {
-
-    //Grabs the username and password from the user.
-    const username = req.body.username;
-    const password = req.body.password;  
-
-    //Try to find the user, Catch if the user is not in the database.
-    try {
-        
-        //Select the user's username from the users database.
-        const result = await db.query("SELECT * FROM users WHERE username = $1", [username.trim()]);
-
-        //Try & Catch if there is a problem getting the password from the database.
-        try { 
-            
-            //If there is a result from the database, check the password associated with the username.
-            if(result.rows.length > 0){
-                
-                const user = result.rows[0];
-
-                //The database password.
-                const databasePassword = user.password;
-
-                //Compare the database password with the user's password.
-                if(password.trim() === databasePassword){
-                    
-                    //The reviewer becomes the user stored in the database. 
-                    reviewer = user.username;
-                    
-                    //Render the homepage.ejs with the user.
-                    res.render("homepage.ejs", {
-                        username: reviewer
-                    });
-                }
-
-                //They do not match, wrong password.
-                else {
-                    res.send("Wrong password.")
-                }
-            }
-
-            //Else, no result, user not found.
-            else{
-                res.send("Can't find that User.");
-            }
-
-        } catch (error) {
-            res.send("Error on getting password.", error)
-        }
-
-    } catch (error) {
-        res.send("Error finding user in database.", error);
-    }
- 
-});
-
-//Create post request.
-app.post("/create", async(req, res) => {
-
-    //Grabs the username and password from the user.
-    const username = req.body.username.trim();
-    const password = req.body.password.trim();  
-
-    //Try Catch checks if the user is in the database.
-    try {
-
-        const result = await db.query("SELECT * FROM users WHERE username = $1", [username]);
-
-        //If there are no results, add the user and password to the users database.
-        if(result.rows.length === 0){
-            await db.query(
-                "INSERT INTO users(username, password) VALUES ($1, $2)",
-                [username, password]
-            );
-
-            //The reviewer becomes the user's name. 
-            reviewer = username;
-
-            //Render the homepage.ejs with the user.
-            res.render("homepage.ejs", {
-                username: reviewer
-            });
-        }
-
-        //Else, the user already exist in the database.
-        else {
-            res.send("User already exist.");
-        }
-        
-    } catch (error) {
-        res.send("Error adding credentials into database", error);
-    }
-
-});
-
 //Add post request.
 app.post("/add", async(req, res) => {
 
@@ -300,7 +305,7 @@ app.post("/add", async(req, res) => {
     //Try to get the user's id from the users database. Catch any errors accessing the database.
     try {
 
-        const result = await db.query("SELECT id FROM users WHERE username = $1", [reviewer]);
+        const result = await db.query("SELECT id FROM users WHERE username = $1", [req.user.username]);
 
         //Gets user's id. 
         const id = result.rows[0].id;
@@ -309,11 +314,11 @@ app.post("/add", async(req, res) => {
         try {
 
             await db.query("INSERT INTO reviews (user_id, username, book_cover, review, title, author, rating) VALUES ($1, $2, $3, $4, $5, $6, $7)", 
-            [id, reviewer, cover, review, title, author, rating]);
+            [id, req.user.username, cover, review, title, author, rating]);
 
             //Render the homepage.ejs with the user, cover, author, and title for the book.
             res.render("homepage.ejs", {
-                username: reviewer,
+                username: req.user.username,
                 author: author,
                 title: title,
                 cover: cover  
@@ -339,7 +344,7 @@ app.post("/sort", async(req, res) => {
         try {
     
             //This query gets the results of all reviews from the selected user and sorts them.
-            const results = await db.query(`SELECT * FROM reviews WHERE username = $1 ORDER BY ${type} ${orderBy}`, [reviewer]);
+            const results = await db.query(`SELECT * FROM reviews WHERE username = $1 ORDER BY ${type} ${orderBy}`, [req.user.username]);
 
             //Empty reviews array that will hold all the results.
             const reviews = [];
@@ -351,7 +356,7 @@ app.post("/sort", async(req, res) => {
     
             //Render the reviews.ejs page with the username and the reviews.
             res.render("reviews.ejs", {
-                username: reviewer,
+                username: req.user.username,
                 reviews: reviews
             });
            
@@ -370,7 +375,7 @@ app.post("/edit", async (req, res) => {
     try {
         
         //Update the review of the user and where the review ID is located.
-        await db.query("UPDATE reviews SET review = $1 WHERE review_id = $2 AND username = $3", [item.review, item.edit, reviewer]);
+        await db.query("UPDATE reviews SET review = $1 WHERE review_id = $2 AND username = $3", [item.review, item.edit, req.user.username]);
 
         //Redirect to reviews.ejs
         res.redirect("/reviews");
@@ -391,7 +396,7 @@ app.post("/delete", async(req, res) => {
     try {
         
         //Delete the review of the user and where the review ID is located.
-        await db.query("DELETE FROM reviews WHERE username = $1 AND review_id = $2", [reviewer, reviewID]);
+        await db.query("DELETE FROM reviews WHERE username = $1 AND review_id = $2", [req.user.username, reviewID]);
 
         //Redirect to reviews.ejs
         res.redirect("/reviews");
@@ -400,6 +405,69 @@ app.post("/delete", async(req, res) => {
         res.send("Error in deleting review from database.", error);
     }
 
+});
+
+passport.use(new Strategy(async function verify(username, password, cb){
+    
+    console.log(username + " " + password);
+
+    //Try to find the user, Catch if the user is not in the database.
+    try {
+        
+        //Select the user's username from the users database.
+        const result = await db.query("SELECT * FROM users WHERE username = $1", [username.trim()]);
+
+            //If there is a result from the database, check the password associated with the username.
+            if(result.rows.length > 0){
+                
+                //User founded in database.
+                const user = result.rows[0];
+
+                //The database password.
+                const databasePassword = user.password;
+                
+                //Compare the database password with the user's password.
+                bcrypt.compare(password, databasePassword, (err, result) => {
+                    
+                    //If there is a error comparing the passwords, return the error.
+                    if(err){
+                        return cb(err);
+                    }
+
+                    //Else, check if the passwords match.
+                    else{
+                        
+                        //If the passwords match, return the user.
+                        if(result){          
+                            return cb(null, user); 
+                        }
+  
+                        //Else, the passwords do not match.
+                        else{
+                            return cb(null, false);
+                        }
+                    }
+                });
+
+            }
+
+            //Else, no result, user not found.
+            else{
+                return cb("User not found");
+            }   
+
+    } catch (error) {
+        return cb(err);
+    }
+
+}));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
 });
 
 //Listening Port.
