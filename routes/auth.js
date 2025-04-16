9336b97e6fccbccce815a863fd5a77179c57a2e0
George <drumerboygt.gt@gmail.com>
2025-04-15 20:43:41 -0400

    Added Google Authentification

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/bookreview',
+  callbackURL: 'https://book-review-capstone-project-2.onrender.com/auth/google/bookreview',
 },
   async (accessToken, refreshToken, profile, done) => {
 
